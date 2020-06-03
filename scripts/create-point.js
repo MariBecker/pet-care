5c01d4386ee992308b3264a65b392e141a3f2867
Mari Becker <mary12_becker@hotmail.com>
2020-06-03 11:13:00 -0300

    Novas variáveis

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -15,8 +15,14 @@ populateUfs();
 
 function getCities(event) {
   const citySelect = document.querySelector("select[name=city]");
+  const stateInput = document.querySelector("input[name=state]");
 
   const ufValue = event.target.value;
+
+  const indexOfSelectState = event.target.selectedIndex
+
+
+  stateInput.value = event.target.options[indexOfSelectState].text
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
