2ab3c28b2498e95841e82d106fa1b1155829a529
Mari Becker <mary12_becker@hotmail.com>
2020-06-09 13:05:51 -0300

    Iniciando tamplate engine

--- src/server.js
+++ src/server.js
@@ -4,15 +4,26 @@ const server = express();
 //Configurar pasta publica
 server.use(express.static("public"));
 
+//Utilizando tamplate engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+
 //Configurar caminhos da minha aplicação
 //Página inicial
 
 server.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  return response.render("index.html");
 });
 
 server.get("/create-point", (request, response) => {
-    response.sendFile(__dirname + "/views/create-point.html");
+  return response.render("create-point.html");
+});
+
+server.get("/search", (request, response) => {
+    return response.render("search-results.html");
   });
 
 //Ligar o servidor
