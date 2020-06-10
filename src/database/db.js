0207267b48ab7cd5e50b5c367c356855b8558e05
Mari Becker <mary12_becker@hotmail.com>
2020-06-10 18:41:47 -0300

    Alterando design

---
+++ src/database/db.js
@@ -0,0 +1,7 @@
+//Importar a depéndência do sqlite3
+
+const sqlite3 = require("sqlite3").verbose();
+
+//Criar o obejto que irá fazer operações no bando de dados
+
+const db = new sqlite3.Database("./src/database/database.db");
